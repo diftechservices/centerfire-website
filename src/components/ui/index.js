f8e3f8ebc7cb87b3d3a875e15882d3c3317e4d3e
Centerfire Dev <noreply@centerfiredigital.com>
2025-09-14 18:43:49 +0000

    feat: Restructure project into frontend and backend directories

--- src/components/ui/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Button } from './Button.jsx'
-export { default as Badge } from './Badge.jsx'
-export { default as Card } from './Card.jsx'
-export { default as StatusIndicator } from './StatusIndicator.jsx'
-export { default as ProgressBar } from './ProgressBar.jsx'
